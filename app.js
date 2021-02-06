bdf1af457daa03bffed219d4734f58fc6410759d
Hayden <haydenkd@protonmail.com>
2021-02-06 00:59:28 -0800

    generate webpage and modularize code

--- app.js
+++ app.js
@@ -1,2 +1,14 @@
+const fs = require("fs");
+const generatePage = require("./src/generate-portfolio");
+
 const profileDataArr = process.argv.slice(2, process.argv.length);
-profileDataArr.forEach(profileItem => console.log(profileItem));
+// profileDataArr.forEach(profileItem => console.log(profileItem));
+
+const firstName = profileDataArr[0];
+const github  = profileDataArr[1];
+
+fs.writeFile("index.html", generatePage(firstName, github), err => {
+    if (err) throw err;
+
+    console.log("Portfolio complete! Check out index.html to see the output!");
+})
